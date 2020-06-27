9ed8b7fe16bb800f6a4237dd1b06eafedd108d2b
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2020-06-27 22:53:20 +0300

    Update Player.java

--- src/GameObject/Player.java
+++ src/GameObject/Player.java
@@ -1,13 +1,10 @@
 package GameObject;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-
 import java.awt.Image;
-
 import Controller.Controller;
 import ImageLoader.ImageLoader;
 
@@ -22,10 +19,9 @@ public class Player implements GameObject{
     private Vector2 position;
     private Vector2 size;
     private Rectangle collisionRect;
-
     private boolean moveWidthDirection=false;
 
-    public Player(float xKord, float yKord, float width, float height,Controller controller){
+    public Player(float xKord, float yKord, float width, float height,Controller controller){//Player sınıfının Constructors bloğu
         this.controller=controller;
         this.xKord=xKord;
         this.yKord=yKord;
@@ -35,39 +31,28 @@ public class Player implements GameObject{
         size=new Vector2(width,height);
         direction=new Vector2();
         collisionRect=new Rectangle(position.x,position.y,size.x,size.y);
-
     }
-
     public void render(SpriteBatch sb) {
          sb.begin();
-         sb.draw(ImageLoader.playerRegion,xKord,yKord,width,height);
-
+         sb.draw(ImageLoader.playerRegion,xKord,yKord,width,height);//playerin çizdirildiği yer
          sb.end();
     }
-
-    public void update(float delta) {
-
+    public void update(float delta) {//player'ın değerlerinin oyun akışına göre değiştiği metod 
         collisionRect.x=xKord;
         collisionRect.y=yKord;
         collisionRect.width=size.x;
         collisionRect.height=size.y;
-
-        if (moveWidthDirection){
-            xKord = controller.getEndKord().x;
-            yKord = controller.getEndKord().y;
+        if (moveWidthDirection){//kullanıcı hareket ettiriyorsa
+            xKord = controller.getEndKord().x;//x koordinatını kullanıcının dokunduğu koordinata eşitler
+            yKord = controller.getEndKord().y;//y koordinatını kullanıcının dokunduğu koordinata eşitler
         }
-
      }
-
-
     public void setMoveWidthDirection(boolean moveWidthDirection) {
         this.moveWidthDirection = moveWidthDirection;
     }
-
     public Rectangle getCollisionRect() {
         return collisionRect;
     }
-
     public void setCollisionRect(Rectangle collisionRect) {
         this.collisionRect = collisionRect;
     }
