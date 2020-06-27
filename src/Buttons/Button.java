1bb8f44d74ebd6d4cd8fec198bc90dfd736bd354
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2020-06-27 21:46:37 +0300

    Update Button.java

--- src/Buttons/Button.java
+++ src/Buttons/Button.java
@@ -7,11 +7,11 @@ import GameObject.GameObject;
 import ImageLoader.ImageLoader;
 
 public class Button implements GameObject {
-    private Rectangle buttonRect;
+    private Rectangle buttonRect;// Rectangle sınıfından buton için bir çerçeve oluşturuyoruz
     private TextureRegion buttonImages;
-    private float xKord,yKord,width,height;
+    private float xKord,yKord,width,height;//butonun boyut ve konum bilgileri için değişkenler tanımlıyoruz
     
-public Button(float xKord,float yKord,TextureRegion buttonImages){
+public Button(float xKord,float yKord,TextureRegion buttonImages){// Buton sınıfı için bir yapıcı method oluşturuyoruz(sınıf çağırıldığında gerekli parametreler girilmelidir!)
     this.buttonImages=buttonImages;
     this.xKord=xKord;
     this.yKord=yKord;
@@ -19,17 +19,17 @@ public Button(float xKord,float yKord,TextureRegion buttonImages){
         height=200;
         buttonRect=new Rectangle(xKord,yKord,width,height);
 }
-public void render(SpriteBatch sb) {
+public void render(SpriteBatch sb) {//libgdx'in oyun ekranının çizdirildiği method
         sb.begin();
         sb.draw(buttonImages,xKord,yKord,width,height);
         sb.end();
     }
-    public void update(float delta) {
+ public void update(float delta) {
     }
-    public Rectangle getButtonRect() {
+ public Rectangle getButtonRect() {
         return buttonRect;
     }
-    public void setButtonRect(Rectangle buttonRect) {
+ public void setButtonRect(Rectangle buttonRect) {
         this.buttonRect = buttonRect;
     }
 }
