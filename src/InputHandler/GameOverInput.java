90755b293fc62761b0b0bc7a4c3e3d0188ba0361
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2020-06-28 12:38:36 +0300

    Update GameOverInput.java

--- src/InputHandler/GameOverInput.java
+++ src/InputHandler/GameOverInput.java
@@ -1,7 +1,5 @@
 package InputHandler;
-
 import com.badlogic.gdx.InputProcessor;
-
 import States.GameOverState;
 import States.MenuState;
 import States.StateManeger;
@@ -9,51 +7,40 @@ import States.StateManeger;
 public class GameOverInput implements InputProcessor {
 
     private GameOverState gameOverState;
-
     public GameOverInput(GameOverState gameOverState) {
         this.gameOverState = gameOverState;
     }
-
     @Override
     public boolean keyDown(int keycode) {
         return false;
     }
-
     @Override
     public boolean keyUp(int keycode) {
         return false;
     }
-
     @Override
     public boolean keyTyped(char character) {
         return false;
     }
-
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) { //game over ekranından menu ekranına geçiş yapan kısım
         gameOverState.getSm().pushState(new MenuState(gameOverState.getSm()));
         return false;
     }
-
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         return false;
     }
-
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
-
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
-
     @Override
     public boolean scrolled(int amount) {
         return false;
     }
-
 }
