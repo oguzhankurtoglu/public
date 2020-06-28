71cec4f963c6430ecbba35c8e6af9ce303b2576a
oguzhankurtoglu <66673273+oguzhankurtoglu@users.noreply.github.com>
2020-06-28 12:58:12 +0300

    Update PracticeStatesInput.java

--- src/InputHandler/PracticeStatesInput.java
+++ src/InputHandler/PracticeStatesInput.java
@@ -1,36 +1,27 @@
-package InputHandler;
-
-
+package InputHandler;//PLAYSTATENIN AYNISIDIR
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
-
 import States.MenuState;
 import States.PlayState;
 import States.PracticeState;
-
 public class PracticeStatesInput implements InputProcessor {
 
     private PracticeState practiceState;
-
     public PracticeStatesInput(PracticeState practiceState){
         this.practiceState=practiceState;
     }
-
     @Override
     public boolean keyDown(int keycode) {
         return false;
     }
-
     @Override
     public boolean keyUp(int keycode) {
         return false;
     }
-
     @Override
     public boolean keyTyped(char character) {
         return false;
     }
-
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 
@@ -38,7 +29,6 @@ public class PracticeStatesInput implements InputProcessor {
         practiceState.getPracticeWorld().getPlayer().setMoveWidthDirection(true);
         return false;
     }
-
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 
@@ -47,21 +37,17 @@ public class PracticeStatesInput implements InputProcessor {
         }
         practiceState.getPracticeWorld().getController().setClick(false);
         practiceState.getPracticeWorld().getPlayer().setMoveWidthDirection(false);
-        practiceState.getPracticeWorld().getController().setEndKord(new Vector2(practiceState.getPracticeWorld().getController().getEndKord()));//farklı platformlar için koordinatları Gdx.graphics.getWidth methodu ile alabiliriz
-
+        practiceState.getPracticeWorld().getController().setEndKord(new Vector2(practiceState.getPracticeWorld().getController().getEndKord()));
         return false;
     }
-
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
-
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
-
     @Override
     public boolean scrolled(int amount) {
         return false;
